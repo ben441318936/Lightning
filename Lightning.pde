95060d203354eddd332c0dff819429b2cf9f7b3d
unknown <jihuang65@697-334-Andunie.sf.k12.internal>
2014-09-04 10:35:36 -0700

    displays hit percentage after 10000 lightnings

--- Lightning.pde
+++ Lightning.pde
@@ -1,13 +1,52 @@
+int x1=0;
+int y1=0;
+int x2=0;
+int y2=0;
+float hit = 0;
+float lightnings = 0;
 void setup()
 {
-  size(300,300);
+	size(500,500);
+	background(0);
 }
 void draw()
 {
-
+	background(0);
+	for (int x=0;x<20;x++)
+	{
+		stroke(random(255),random(255),random(255));
+		while (x1 < 500)
+		{
+			x2 = x2+(int)(Math.random()*15)+1;
+			y2 = y2+(int)(Math.random()*15)+1;
+			line(x1,y1,x2,y2);
+			x1 = x2;
+			y1 = y2;	
+		}
+		lightnings++;
+		if (x2==500 && y2==500)
+		{
+			hit++;
+		}
+		x1 = 0;
+		x2 = 0;
+		y1 = 0;
+		y2 = 0;	
+	}
+	if (lightnings == 10000)
+	{
+		println("number of lightnings " + lightnings);
+		println("nuber of hits at (500,500) " + hit);
+		println("hit percentage " + (hit/lightnings)*100);
+		noLoop();
+	}
 }
+
 void mousePressed()
 {
-
+	
+	hit=0;
+	lightnings=0;
+	redraw();
 }
 
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -14,19 +14,58 @@ import java.io.IOException;
 
 public class Lightning extends PApplet {
 
+int x1=0;
+int y1=0;
+int x2=0;
+int y2=0;
+float hit = 0;
+float lightnings = 0;
 public void setup()
 {
-  size(300,300);
-  noLoop();
+	size(500,500);
+	background(0);
 }
 public void draw()
 {
-  int x = (int)(Math.random() * 300);
-  ellipse(x,150,10,10);
+	background(0);
+	for (int x=0;x<20;x++)
+	{
+		//fill(random(255),random(255),random(255));
+		//ellipse(250, 250, 250, 250);
+		stroke(random(255),random(255),random(255));
+		while (x1 < 500)
+		{
+			x2 = x2+(int)(Math.random()*15)+1;
+			y2 = y2+(int)(Math.random()*15)+1;
+			line(x1,y1,x2,y2);
+			x1 = x2;
+			y1 = y2;	
+		}
+		lightnings++;
+		if (x2==500 && y2==500)
+		{
+			hit++;
+		}
+		x1 = 0;
+		x2 = 0;
+		y1 = 0;
+		y2 = 0;	
+	}
+	if (lightnings == 10000)
+	{
+		println("number of lightnings " + lightnings);
+	println("nuber of hits at (500,500) " + hit);
+	println("hit percentage " + (hit/lightnings)*100);
+		noLoop();
+	}
 }
+
 public void mousePressed()
 {
-  redraw();
+	
+	hit=0;
+	lightnings=0;
+	redraw();
 }
 
   static public void main(String[] passedArgs) {
